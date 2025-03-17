df06e58b28799dfdfde088b99b035ee37782edf1
sagarboyal <sboyal12@gmail.com>
2025-03-17 19:36:48 +0530

    ui done

--- redux-project/src/PostList.jsx
+++ redux-project/src/PostList.jsx
@@ -10,9 +10,37 @@ const PostList = () => {
         dispath(fetchPosts());
     }, [dispath]);
     
+    if(loading) return <h2>Loading...</h2>
+    if(error) return (
+        <>
+            <h2 style={{color: "red", fontSize: "40px"}}>Error 404</h2>
+            <p style={{marginTop: "-30px"}}>{error}</p>
+        </>
+    )
     return (
         <div>
-        <h1>Post List</h1>
+            <h1 style={{color: "whitesmoke"}}>Posts</h1>
+            <ul>
+                {post.map(post => (
+                    <li key={post.id} 
+                        style={{
+                            border: "2px solid lightblue", padding: "10px",
+                            margin: "5px",
+                            borderRadius: "10px"}}>
+                        <h3 
+                            style={{
+                                fontSize: "25px", 
+                                color: "lightblue"}}>
+                            {post.title}
+                        </h3>
+                        <p 
+                            style={{
+                                color: "cadetblue"}}>
+                            {post.body}
+                        </p>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
