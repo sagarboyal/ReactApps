847f6de0699881357d39c8e38114d187f12cee6e
sagarboyal <sboyal12@gmail.com>
2025-03-18 11:20:50 +0530

    product setup done

--- j-commerce/src/App.jsx
+++ j-commerce/src/App.jsx
@@ -3,11 +3,8 @@ import './App.css'
 
 
 function App() {
-  toast.success('Successfully toasted!');
-  toast.error("This didn't work.")
   return (
     <div className='flex justify-center items-center h-screen bg-gray-800 text-white text-2xl font-bold'>
-      <Toaster/>
       <FaBeer />
     </div>
   )
