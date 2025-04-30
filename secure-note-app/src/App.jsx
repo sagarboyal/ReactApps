9bc81afc96339e9ce23cf5044d2b582f9634fc9e
sagarboyal <sboyal12@gmail.com>
2025-04-30 11:27:56 +0530

    deps & page: html-to-text deps | about page | tailwind some css added

--- secure-note-app/src/App.jsx
+++ secure-note-app/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import AboutPage from "./components/about-page/AboutPage";
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <AboutPage />
     </Router>
   );
 }
