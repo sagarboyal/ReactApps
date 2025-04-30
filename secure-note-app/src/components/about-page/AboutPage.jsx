4b860655c835883762f7ec4c22d301a8aed740f8
sagarboyal <sboyal12@gmail.com>
2025-04-30 11:36:43 +0530

    feat: about page | fonts added

--- secure-note-app/src/components/about-page/AboutPage.jsx
+++ secure-note-app/src/components/about-page/AboutPage.jsx
@@ -10,10 +10,12 @@ import {
 
 const AboutPage = () => {
   return (
-    <div className=' p-8   bg-gray-100 min-h-screen'>
-      <div className='md:w-1/2'>
-        <h1 className='text-4xl font-bold mb-4'>About Us</h1>
-        <p className='mb-4'>
+    <div className='bg-gray-100 min-h-screen flex items-center justify-center p-4'>
+      <div className='max-w-6xl w-full p-8 bg-white rounded-lg shadow-lg'>
+        <h1 className='text-4xl font-semibold text-center text-gray-800 mb-6'>
+          About Us
+        </h1>
+        <p className='text-lg text-gray-700 mb-6 leading-relaxed'>
           Welcome to SecureNote, your trusted companion for secure and private
           note-taking. We believe in providing a safe space where your thoughts
           and ideas are protected with the highest level of security. Our
@@ -23,32 +25,52 @@ const AboutPage = () => {
           secure.
         </p>
 
-        <ul className='list-disc list-inside mb-4 text-sm px-6 py-2'>
+        <ul className='list-disc list-inside text-gray-700 mb-6'>
           <li className='mb-2'>
-            Add an extra layer of security with two-factor authentication.
+            <span className='font-semibold'>
+              Add an extra layer of security
+            </span>{" "}
+            with two-factor authentication.
           </li>
           <li className='mb-2'>
-            Your notes are encrypted from the moment you create them
+            <span className='font-semibold'>Your notes are encrypted</span> from
+            the moment you create them.
           </li>
           <li className='mb-2'>
-            Access your notes from anywhere with the assurance that they are
-            stored .
+            <span className='font-semibold'>
+              Access your notes from anywhere
+            </span>{" "}
+            with the assurance that they are stored safely.
           </li>
           <li className='mb-2'>
-            Our app is designed to be intuitive and easy to use.
+            <span className='font-semibold'>Our app is designed</span> to be
+            intuitive and easy to use.
           </li>
         </ul>
-        <div className='flex space-x-4 mt-10'>
-          <Link className='text-white rounded-full p-2 bg-customRed  ' to='/'>
+
+        <div className='flex justify-center space-x-6 mt-8'>
+          <Link
+            className='text-white rounded-full p-3 bg-blue-600 hover:bg-blue-800 transition-colors duration-300'
+            to='/'
+          >
             <FaFacebookF size={24} />
           </Link>
-          <Link className='text-white rounded-full p-2 bg-customRed  ' to='/'>
+          <Link
+            className='text-white rounded-full p-3 bg-blue-500 hover:bg-blue-700 transition-colors duration-300'
+            to='/'
+          >
             <FaTwitter size={24} />
           </Link>
-          <Link className='text-white rounded-full p-2 bg-customRed  ' to='/'>
+          <Link
+            className='text-white rounded-full p-3 bg-blue-700 hover:bg-blue-900 transition-colors duration-300'
+            to='/'
+          >
             <FaLinkedinIn size={24} />
           </Link>
-          <Link className='text-white rounded-full p-2 bg-customRed  ' to='/'>
+          <Link
+            className='text-white rounded-full p-3 bg-pink-600 hover:bg-pink-800 transition-colors duration-300'
+            to='/'
+          >
             <FaInstagram size={24} />
           </Link>
         </div>
